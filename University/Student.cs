6293b00d24cd6397b80aeedaca41da2c6e33ae40
Nicor <nicorbogach@gmail.com>
2025-07-06 07:16:18 +0300

    inheritance of the person class

--- University/Student.cs
+++ University/Student.cs
@@ -1,16 +1,32 @@
 ﻿namespace University;
 
-public class Student
+public class Student:Person
 {
+    public Student(string FirstName, string LastName, int age, Address address) : base(FirstName, LastName, age,
+        address){}
+    public Student(string FirstName, string LastName, string MiddleName, int age, Address address) : base(FirstName, LastName, MiddleName, age,
+        address){}
+    public Student(string FirstName, string LastName, string MiddleName, int age, Address address, string MillitaryId, bool CriminalRecord) : base(FirstName, LastName, MiddleName, age,
+        address, MillitaryId, CriminalRecord){}
     
 }
 
-public class elder:Student
+public class Elder:Student
 {
-    
+    public Elder(string FirstName, string LastName, int age, Address address) : base(FirstName, LastName, age,
+        address){}
+    public Elder(string FirstName, string LastName, string MiddleName, int age, Address address) : base(FirstName, LastName, MiddleName, age,
+        address){}
+    public Elder(string FirstName, string LastName, string MiddleName, int age, Address address, string MillitaryId, bool CriminalRecord) : base(FirstName, LastName, MiddleName, age,
+        address, MillitaryId, CriminalRecord){}
 }
 
-public class proforg:Student
+public class Proforg:Student
 {
-    
+    public Proforg(string FirstName, string LastName, int age, Address address) : base(FirstName, LastName, age,
+        address){}
+    public Proforg(string FirstName, string LastName, string MiddleName, int age, Address address) : base(FirstName, LastName, MiddleName, age,
+        address){}
+    public Proforg(string FirstName, string LastName, string MiddleName, int age, Address address, string MillitaryId, bool CriminalRecord) : base(FirstName, LastName, MiddleName, age,
+        address, MillitaryId, CriminalRecord){}
 }
--- University/obj/Debug/net9.0/University.AssemblyInfo.cs
+++ University/obj/Debug/net9.0/University.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("University")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2b95cc28d75c16f029951f12e2b19decb775ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2150bf3d2253663372d94363169c9387e6acdc2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("University")]
 [assembly: System.Reflection.AssemblyTitleAttribute("University")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
