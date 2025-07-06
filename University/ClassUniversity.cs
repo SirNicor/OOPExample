7c2b95cc28d75c16f029951f12e2b19decb775ea
Nicor <nicorbogach@gmail.com>
2025-07-06 06:48:12 +0300

    Creating the basic structure 2

---
+++ University/ClassUniversity.cs
@@ -0,0 +1,22 @@
+﻿namespace University;
+
+public class ClassUniversity
+{
+
+}
+public class Faculty:ClassUniversity
+{
+    
+}
+public class Department:ClassUniversity
+{
+    
+}
+public class Group:ClassUniversity
+{
+    
+}
+public class Dormitories:ClassUniversity
+{
+    
+}
---
+++ University/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace University;
+
+public class Student
+{
+    
+}
+
+public class elder:Student
+{
+    
+}
+
+public class proforg:Student
+{
+    
+}
