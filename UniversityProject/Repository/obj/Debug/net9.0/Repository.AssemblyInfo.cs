79ab4c7555da597aa78c40972061ceed9fc48dbc
Nicor <nicorbogach@gmail.com>
2025-07-18 13:50:13 +0300

    Refactoring2

--- UniversityProject/Repository/obj/Debug/net9.0/Repository.AssemblyInfo.cs
+++ UniversityProject/Repository/obj/Debug/net9.0/Repository.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79c3cd556272e78bea4225a9c3deeb4373401f48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+477148570cff8e2500c7f78f9d5af480353ebea8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
