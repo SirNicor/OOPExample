e79d37b05f9489d9a7a05745d46fd99e861860b4
Nicor <nicorbogach@gmail.com>
2025-07-29 18:59:13 +0300

    Department repository

---
+++ UniversityProject/Repository/DepartmentRepository.cs
@@ -0,0 +1,54 @@
+﻿namespace Repository;
+using University.UCore;
+using Logger;
+
+public class DepartmentRepository
+{
+    static DepartmentRepository()
+    {
+        _facultyRepository = new FacultyRepository();
+        List<Worker> workerRep = new WorkerRepository().ReturnList();
+        UniversityRepository rep = new UniversityRepository();
+        foreach (var worker in workerRep)
+        {
+            if (worker.GetType() == typeof(Administrator))
+            {
+                _workerRep.Add((Administrator)worker);
+            }
+        } 
+        _departments.Add(new Department("IIST", _workerRep[5],  _workerRep.GetRange(6, 1), _facultyRepository.ReturnList()[0]));
+    }
+
+    public DepartmentRepository()
+    {
+        
+    }
+    
+    public void Add(Department department, Logger logger)
+    {
+        try
+        {
+            _departments.Add(department);
+            logger.Debug("Worker added" + Environment.NewLine);
+        }
+        catch(Exception exception)
+        {
+            logger.Error("Worker not added, The information is incomplete " + Environment.NewLine, exception);
+        }
+    }
+    
+    public List<Department> ReturnList(Logger logger)
+    {
+        logger.Debug("Return list" + Environment.NewLine);
+        return _departments;
+    }
+    
+    public List<Department> ReturnList()
+    {
+        return _departments;
+    }
+
+    private static FacultyRepository _facultyRepository;
+    private static List<Administrator> _workerRep;
+    private static List<Department> _departments;
+}
--- UniversityProject/UCore/Departament.cs
+++ UniversityProject/UCore/Departament.cs
@@ -2,15 +2,16 @@
 
 public class Department
 {
-    public Department(string nameDepartment, Administrator headDepartment, Faculty faculty)
+    public Department(string nameDepartment, Administrator headDepartment, List<Administrator> administrators, Faculty faculty)
     {
         Faculty =  faculty;
         NameDepartment = nameDepartment;
         HeadDepartment = headDepartment;
+        Administrators = administrators;
     }
     
     public readonly Faculty Faculty;
     protected Administrator HeadDepartment;
-    protected Administrator[] Administrators;
+    protected List<Administrator> Administrators;
     public readonly string NameDepartment;
 }
