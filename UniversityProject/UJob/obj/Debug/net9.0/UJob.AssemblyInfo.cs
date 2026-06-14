018eadca2c2bd20187a6bfe182f373e73b786c29
Nicor <nicorbogach@gmail.com>
2026-06-14 17:36:26 +0300

    GitIgnoreRemake

--- UniversityProject/UJob/obj/Debug/net9.0/UJob.AssemblyInfo.cs
+++ UniversityProject/UJob/obj/Debug/net9.0/UJob.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UJob")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd863efceacfd675d3753e7d18abbe33aa6b3e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2676b30219c3d8d9beec02a8106cb05e59446e35")]
 [assembly: System.Reflection.AssemblyProductAttribute("UJob")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UJob")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
